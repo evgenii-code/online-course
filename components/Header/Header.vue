c059ef543aa4f37aca994c32e4fb4699dba5a1d7
Evgenii <osipenkov.evgenii@gmail.com>
2022-01-10 19:57:32 +0300

    feature(components): Add basic header layout

---
+++ components/Header/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header :class="$style.header">
+    <app-container :class="$style.container">
+      <app-header-menu :class="$style.menu" :menu-links="menuLinks" />
+    </app-container>
+  </header>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'AppHeader',
+
+  computed: {
+    ...mapGetters({
+      menuLinks: 'core/menuLinks',
+    }),
+  },
+};
+</script>
+
+<style lang="scss" module>
+@import './Header.module.scss';
+</style>
---
+++ components/Header/Menu/HeaderMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav :class="$style.menu">
+    <ul :class="$style.items">
+      <li :class="$style.item">
+        <nuxt-link :class="$style.link" to="/">link</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMenu',
+
+  props: {
+    menuLinks: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+@import './HeaderMenu.module.scss';
+</style>
