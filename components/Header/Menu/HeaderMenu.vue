18529f46c76824b0be1a8e89d896e7e97a9734d3
Evgenii <osipenkov.evgenii@gmail.com>
2022-01-12 17:16:58 +0300

    enhancements(components): DEV-1 Add menu link items

--- components/Header/Menu/HeaderMenu.vue
+++ components/Header/Menu/HeaderMenu.vue
@@ -1,8 +1,14 @@
 <template>
   <nav :class="$style.menu">
     <ul :class="$style.items">
-      <li :class="$style.item">
-        <nuxt-link :class="$style.link" to="/">link</nuxt-link>
+      <li
+        v-for="menuLink in menuLinks"
+        :key="menuLink.link"
+        :class="$style.item"
+      >
+        <nuxt-link :class="$style.link" :to="menuLink.link">{{
+          menuLink.title
+        }}</nuxt-link>
       </li>
     </ul>
   </nav>
