71c0d57932973856bc1f17d869caf47ae0972275
Evgenii <osipenkov.evgenii@gmail.com>
2022-01-13 16:53:16 +0300

    feature(components): DEV-1 Add header account links

---
+++ components/Header/Account/HeaderAccount.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav :class="$style.account">
+    <v-icon name="profile" :class="$style.icon" />
+
+    <ul :class="$style.items">
+      <li :class="$style.item">
+        <button :class="$style.button">
+          {{ $t('header.logIn') }}
+        </button>
+      </li>
+
+      <li :class="$style.item">
+        <p :class="$style.devider">/</p>
+      </li>
+
+      <li :class="$style.item">
+        <button :class="$style.button">
+          {{ $t('header.register') }}
+        </button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'AppHeaderAccount',
+};
+</script>
+
+<style lang="scss" module>
+@import './HeaderAccount.module.scss';
+</style>
