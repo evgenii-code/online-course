fe132b5151230a6ff3a7746e2bf54f33f3b6d1b6
Evgenii <osipenkov.evgenii@gmail.com>
2022-01-14 19:03:50 +0300

    feature(components): DEV-7 Add Input component

---
+++ components/UI/Input/Input.vue
@@ -0,0 +1,143 @@
+<template>
+  <div :class="classes">
+    <label v-if="label" :for="uuid" :class="$style.label">{{ label }}</label>
+
+    <div :class="$style.wrapper">
+      <input
+        :id="uuid"
+        v-model="internalValue"
+        :type="type"
+        :name="name"
+        v-bind="$attrs"
+        :class="$style.field"
+        :disabled="disabled"
+        v-on="events"
+      />
+
+      <div v-if="hasPostfix" :class="$style.postfix">
+        <slot name="postfix"></slot>
+      </div>
+    </div>
+
+    <div v-if="internalError" :class="$style['error-message']">
+      {{ internalError }}
+    </div>
+  </div>
+</template>
+
+<script>
+import getUID from '~/utils/getUID';
+
+const INPUT_SIZES = ['small', 'regular', 'large'];
+const INPUT_THEME_NAMES = ['light', 'dark'];
+
+export default {
+  name: 'AppInput',
+
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+
+    label: {
+      type: String,
+      default: '',
+    },
+
+    type: {
+      type: String,
+      default: 'text',
+    },
+
+    error: {
+      type: [String, Boolean],
+      default: '',
+    },
+
+    success: {
+      type: Boolean,
+      default: false,
+    },
+
+    name: {
+      type: String,
+      default: '',
+    },
+
+    size: {
+      type: String,
+      default: 'regular',
+      validator: (size) => INPUT_SIZES.includes(size),
+    },
+
+    theme: {
+      type: String,
+      default: 'light',
+      validator: (theme) => INPUT_THEME_NAMES.includes(theme),
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      internalValue: this.value,
+      internalError: this.error,
+    };
+  },
+
+  computed: {
+    events() {
+      return {
+        ...this.$listeners,
+        input: this.onInput,
+      };
+    },
+
+    classes() {
+      return {
+        [this.$style.input]: true,
+        [this.$style[this.size]]: true,
+        [this.$style[this.theme]]: true,
+        [this.$style['has-postfix']]: this.hasPostfix,
+        [this.$style.error]: !!this.internalError,
+        [this.$style.success]: this.success,
+        [this.$style.disabled]: this.disabled,
+      };
+    },
+
+    hasPostfix() {
+      return !!this.$slots.postfix;
+    },
+  },
+
+  watch: {
+    value(value) {
+      this.internalValue = value;
+    },
+
+    error(error) {
+      this.internalError = error;
+    },
+  },
+
+  beforeCreate() {
+    this.uuid = getUID();
+  },
+
+  methods: {
+    onInput(event) {
+      this.internalValue = event.target.value;
+      this.$emit('input', this.internalValue);
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+@import './Input.module.scss';
+</style>
