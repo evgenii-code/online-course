65853869b3d372e0b3b2f532e3529f4814264737
Evgenii <osipenkov.evgenii@gmail.com>
2022-01-21 18:09:00 +0300

    feature(components): DEV-21 Add Badge component

---
+++ components/UI/Badge/Badge.vue
@@ -0,0 +1,41 @@
+<template>
+  <p :class="classes">
+    <slot />
+  </p>
+</template>
+
+<script>
+const BADGE_VARIANTS = [
+  'primary',
+  'secondary',
+  'success',
+  'info',
+  'warning',
+  'alert',
+];
+
+export default {
+  name: 'AppBadge',
+
+  props: {
+    variant: {
+      type: String,
+      default: 'primary',
+      validator: (variant) => BADGE_VARIANTS.includes(variant),
+    },
+  },
+
+  computed: {
+    classes() {
+      return {
+        [this.$style.badge]: true,
+        [this.$style[this.variant]]: true,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+@import './Badge.module';
+</style>
