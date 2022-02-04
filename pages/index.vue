e0da985f50cb60c2cc11fa4d0d23ffb30d98903e
Evgenii <osipenkov.evgenii@gmail.com>
2022-02-04 18:56:37 +0300

    feature(pages): DEV-22 Add basic events section

--- pages/index.vue
+++ pages/index.vue
@@ -39,12 +39,37 @@
         <app-benefits :class="$style.content" />
       </app-container>
     </section>
+
+    <section :class="[$style.section, $style.benefits]">
+      <app-container :class="$style.container">
+        <app-card-event
+          v-for="event in events"
+          :key="event.title"
+          :class="$style.content"
+          :date-start="event.date.start"
+          :date-end="event.date.end"
+          :href="event.link"
+          colored-shadow
+        >
+          <template #title>{{ $t(event.title) }}</template>
+          <template #category>{{ $t(event.category) }}</template>
+        </app-card-event>
+      </app-container>
+    </section>
   </main>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'IndexPage',
+
+  computed: {
+    ...mapGetters({
+      events: 'core/eventsLinks',
+    }),
+  },
 };
 </script>
 
