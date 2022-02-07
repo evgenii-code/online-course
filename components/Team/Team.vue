3c701cfed31d7206468114f66b5c05d27f4e76df
Evgenii <osipenkov.evgenii@gmail.com>
2022-02-07 17:27:28 +0300

    feature(components): DEV-34 Add the basic team memebers component

---
+++ components/Team/Team.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="$style.team">
+    <app-heading :class="$style.heading">
+      <template #title>{{ $t('team.title') }}</template>
+      <template #subtitle>{{ $t('team.subtitle') }}</template>
+    </app-heading>
+
+    <ul :class="$style.members">
+      <li
+        v-for="teamMember in teamLinks"
+        :key="`team-member-${teamMember.id}`"
+        :class="$style.member"
+      >
+        <app-card-team :class="$style.card" :team-member="teamMember" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'AppTeam',
+
+  computed: {
+    ...mapGetters({
+      teamLinks: 'core/teamLinks',
+    }),
+  },
+};
+</script>
+
+<style lang="scss" module>
+@import './Team.module';
+</style>
