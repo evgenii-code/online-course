dee67409738e34e33fd9277fc0684899afc4d86c
Evgenii <osipenkov.evgenii@gmail.com>
2022-02-07 19:39:49 +0300

    feature(components): DEV-34 Add control button component

---
+++ components/UI/Button/Control/ButtonControl.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-button-clear :class="$style.button" v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </v-button-clear>
+</template>
+
+<script>
+export default {
+  name: 'AppButtonControl',
+};
+</script>
+
+<style lang="scss" module>
+@import './ButtonControl.module';
+</style>
