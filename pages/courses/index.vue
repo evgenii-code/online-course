32d8ef905b3486cc43e78d2d9e66cc00a151b697
Evgenii <osipenkov.evgenii@gmail.com>
2022-02-15 19:16:38 +0300

    feature(pages): DEV-40 Add sections to the courses page

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -29,6 +29,44 @@
         <app-courses-all :class="$style.content" />
       </app-container>
     </section>
+
+    <section :class="[$style.section, $style.testimonials]">
+      <app-container :class="$style.container">
+        <app-testimonials :class="$style.content" />
+      </app-container>
+    </section>
+
+    <section :class="[$style.section, $style.certificate]">
+      <app-container :class="$style.container">
+        <app-certificate :class="$style.content" />
+      </app-container>
+    </section>
+
+    <section :class="[$style.section, $style.subscribe]">
+      <img
+        src="~/assets/images/subscribe/illustration-1.svg"
+        alt="Background image"
+        aria-hidden="true"
+        role="none"
+        :class="[$style.image, $style.illustration, $style.left]"
+      />
+      <img
+        src="~/assets/images/subscribe/illustration-1.svg"
+        alt="Background image"
+        aria-hidden="true"
+        role="none"
+        :class="[$style.image, $style.illustration, $style.right]"
+      />
+
+      <app-container :class="$style.container">
+        <app-subscribe :class="$style.content">
+          <template #title>{{ $t('subscribe.announcements.title') }}</template>
+          <template #subtitle>{{
+            $t('subscribe.announcements.subtitle')
+          }}</template>
+        </app-subscribe>
+      </app-container>
+    </section>
   </main>
 </template>
 
