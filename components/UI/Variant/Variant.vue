ef7f37098bd2f5f1d95408f045d92d28c14c01d8
Evgenii <osipenkov.evgenii@gmail.com>
2022-02-16 15:10:14 +0300

    feature(components): DEV-45 Add Variant radio component

---
+++ components/UI/Variant/Variant.vue
@@ -0,0 +1,94 @@
+<template>
+  <label :class="$style.variant" :for="uuid">
+    <input
+      :id="uuid"
+      v-model="model"
+      :type="inputType"
+      :value="currentValue"
+      v-bind="$attrs"
+      :class="$style.input"
+    />
+
+    <span :class="$style.content">
+      <slot name="custom" :isChecked="isChecked">
+        <!-- TODO - make custom radio && checkbox check mark -->
+        <span :class="$style.mark" />
+      </slot>
+
+      <span :class="$style.label">
+        <slot name="label" />
+      </span>
+    </span>
+  </label>
+</template>
+
+<script>
+import getUID from '~/utils/getUID';
+
+export default {
+  name: 'AppRadioButton',
+
+  model: {
+    prop: 'modelValue',
+    event: 'change',
+  },
+
+  props: {
+    modelValue: {
+      type: [Array, String],
+      required: true,
+    },
+
+    currentValue: {
+      type: String,
+      required: true,
+    },
+
+    checkedTheme: {
+      type: String,
+      default: 'primary',
+    },
+
+    notCheckedTheme: {
+      type: String,
+      default: 'clear',
+    },
+  },
+
+  computed: {
+    inputType() {
+      return this.multiple ? 'checkbox' : 'radio';
+    },
+
+    multiple() {
+      return Array.isArray(this.modelValue);
+    },
+
+    model: {
+      get() {
+        return this.modelValue;
+      },
+
+      set(currentValue) {
+        this.$emit('change', currentValue);
+      },
+    },
+
+    isChecked() {
+      if (this.multiple) {
+        return this.modelValue.includes(this.currentValue);
+      }
+
+      return this.modelValue === this.currentValue;
+    },
+  },
+
+  beforeCreate() {
+    this.uuid = getUID();
+  },
+};
+</script>
+
+<style lang="scss" module>
+@import './Variant.module';
+</style>
