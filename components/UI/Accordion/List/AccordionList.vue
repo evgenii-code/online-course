e1913126e8c586e8294ea8fa3c6378a7bb4e7524
Evgenii <osipenkov.evgenii@gmail.com>
2022-03-02 15:41:47 +0300

    fix(components): DEV-53 Fix accordion button tag

--- components/UI/Accordion/List/AccordionList.vue
+++ components/UI/Accordion/List/AccordionList.vue
@@ -7,10 +7,10 @@
       :class="$style.item"
     >
       <template #title>
-        <h3 :class="$style.title">
+        <p :class="$style.title">
           <b v-if="item.prefix" :class="$style.prefix">{{ item.prefix }}</b
           >{{ item.title }}
-        </h3>
+        </p>
       </template>
 
       <template #content>
